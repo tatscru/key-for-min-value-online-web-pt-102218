6e41a189beecac1c42c2d666a0778b0d91dd5f03
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 20:24:23 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+# ikea = {bed => 500, chair => 30, plant => 20}
 
+def key_for_min_value(name_hash)
+  lowest_price = nil 
+  cheapest_item = nil
+  
+  name_hash.each do |item, price|
+    if lowest_price == nil || price < lowest_price
+      lowest_price = price 
+      cheapest_item = item 
+    end 
+  end 
+ cheapest_item
 end
+
+# The first time through, lowest_price is nil, and price is 25. You have to check whether lowest_price is nil, OR whether price is less than lowest_price. 
